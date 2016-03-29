0bbf5371d1a855c85e46d094cd5c6a66fa7bace8
Esteban Dorado <fadoro@gmail.com>
2016-03-29 18:12:57 +0200

    Exercises 2,4,6,8,10 done

--- Ejercicios/Soluciones/ejercicio2.scala
+++ Ejercicios/Soluciones/ejercicio2.scala
@@ -6,8 +6,17 @@ Pista: filter y foreach
 */
 
 object ejercicio2 {
-	def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit = {
 
-		// Your code here
-	}
+    // Your code here
+    number_odd()
+    number_odds_unit()
+    number_odd_generator()
+  }
+
+  def number_odd_generator(): Unit = for (n <- 1 to 100 if n % 2 == 1) println(n)
+
+  def number_odd(): Unit = (1 to 100).filter(_ % 2 == 1).foreach(n => println(n))
+
+  def number_odds_unit(): Unit = (1 to 100).filter(_ % 2 == 1).foreach(println)
 }
