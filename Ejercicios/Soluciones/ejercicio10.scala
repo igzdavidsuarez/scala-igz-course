f24fdfd7e571e4f5445a842cb233664b54b7328a
David Suarez <david.suarez@intelygenz.com>
2016-04-01 09:48:29 +0200

    Exercise 10 solved two ways

--- Ejercicios/Soluciones/ejercicio10.scala
+++ Ejercicios/Soluciones/ejercicio10.scala
@@ -1,5 +1,6 @@
 /*
 ENUNCIADO:
+<<<<<<< Updated upstream
 Implementa en	Scala	un programa que permita construir
 parkímetros utilizando estado	local	(al	estilo del ejemplo del
 contador con estado	y	mensajes). La	clausura devuelta tendrá el
@@ -12,38 +13,97 @@ Mensajes y acciones	a	realizar:
 ●	"Tiempo": paraconsultar el	tiempo que le queda
 ●	"total": para	saber	cuánto dinero	ha recogido	un determinado parkimetro. ● "tick":	El tiempo
 se decrementa manualmente simulando	el tick	de reloj.
+=======
+Implementa	en	Scala	un	programa que permita construir
+parkímetros utilizando estado	local	(al	estilo	del	ejemplo	del	
+contador	con	estado	y	mensajes).	La	clausura devuelta tendra	el	
+perfil	(String,Double)=>Double,	siendo	el	String	el	mensaje que
+especifica	la	acción	a	hacer por	el	parkímetro,	el	Double	el	
+parámetro necesario	en	algún mensaje.	En	el	caso que	no	
+necesite	el	parámetro,	se	pasara	un	valor	por defecto como	0.0.	
+Mensajes	y	acciones	a	realizar:	
+●	"pagar":	se	añade dinero	al	parkímetro.	Cuesta	0.01	euros/minuto.	
+●	"tiempo":	paraconsultar	el	tiempo que	le	queda
+●	"total":	para	saber	cuánto dinero	ha	recogido	un	determinado parkimetro.
+●	"tick":	El	tiempo se	decrementa manualmente simulando	el	tick	de	reloj.
+>>>>>>> Stashed changes
 */
 
 
 object ejercicio10 {
   def main(args: Array[String]): Unit = {
 
-    // Your code here
+    // Function
     println(makeParkimetro()("pagar", 100))
     println(makeParkimetro()("tiempo", 100))
     println(makeParkimetro()("total", 100))
     println(makeParkimetro()("tick", 100))
+
+	// Case Classes
+	val park = parquimetro(5, 10)
+	park.enviarAccion("tiempo", 0.0)
+	park.enviarAccion("total", 0.0)
+	park.enviarAccion("pagar", 20)
+	park.enviarAccion("tiempo", 0.0)
+	park.enviarAccion("total", 0.0)
+	park.enviarAccion("pagar", 20)
+	park.enviarAccion("tiempo", 0.0)
+	park.enviarAccion("total", 0.0)
+	park.enviarAccion("tick", 0.0)
+	park.enviarAccion("tick", 0.0)
+	park.enviarAccion("tick", 0.0)
+	park.enviarAccion("tiempo", 0.0)
+	park.enviarAccion("total", 0.0)
   }
 
-  def makeParkimetro(): (String, Double) => Double = {
-    var dineroAcc: Double = 0
-    var tiempo: Double = 0
-    (accion: String, cantidad: Double) => {
-      accion match {
-        case "pagar" => {
-          tiempo += cantidad / 0.01
-          dineroAcc += cantidad
-          tiempo
-        }
-        case "tiempo" => tiempo
-        case "total" => dineroAcc
-        case "tick" => {
-          if (tiempo > 0) {
-            tiempo = tiempo - 1
-          }
-          tiempo
-        }
-      }
-    }
-  }
+	def makeParkimetro(): (String, Double) => Double = {
+		var dineroAcc: Double = 0
+		var tiempo: Double = 0
+		(accion: String, cantidad: Double) => {
+		  accion match {
+		    case "pagar" => {
+		      tiempo += cantidad / 0.01
+		      dineroAcc += cantidad
+		      tiempo
+		    }
+		    case "tiempo" => tiempo
+		    case "total" => dineroAcc
+		    case "tick" => {
+		      if (tiempo > 0) {
+		        tiempo = tiempo - 1
+		      }
+		      tiempo
+		    }
+		  }
+		}
+	}
+
+	case class parquimetro(var time: Double, var total: Double) {
+		def enviarAccion(accion: String, cantidad: Double): Unit = {
+			accion match {
+				case "pagar" => pagarParquimetro(cantidad)
+				case "tiempo" => consultaTiempo()
+				case "total" => consultarDinero()
+				case "tick" => decrementarTiempo()
+				case _ => 
+			}
+		}
+
+		def pagarParquimetro(cantidad: Double): Unit = {
+			time = time + (cantidad/0.01 * 60)
+		}
+
+		def consultaTiempo(): Unit = {
+			println("Time remaining: " + time)
+		}
+
+		def consultarDinero(): Unit = {
+			println("Cash accumulated: " + total)
+		}
+
+		def decrementarTiempo(): Unit = {
+			time = time - 1
+			total = total + 0.01/60
+		}
+	}
 }
